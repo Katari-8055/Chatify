130e009fb7703269b587bc07a0513851fde8eedb
Katari <kataricoder@gmail.com>
2025-12-19 02:49:06 +0530

    implement user signup functionality with token generation and password hashing

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,60 @@
+import User from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
+import { asynHandler } from "../utils/asyncHandler.js";
+import ApiResponse from "../utils/ApiResponce.js";
+
+
+const options = {
+        httpOnly: true,
+        secure: true
+}
+
+const generateAccessToken = async (id) =>{
+    try {
+        const user = await User.findById(id);
+        const accessToken = user.generateToken();
+        return {accessToken};
+    } catch (error) {
+        throw new ApiError(400, "Unable To Generate Token")
+    }
+}
+
+export const signUp = asynHandler(async (req, res)=>{
+    const {email, fullName, password} = req.body;
+    if(!email || !fullName || !password){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    if(password.length < 6){
+        throw new ApiError(400, "Password must be at least 6");
+    }
+
+    const user = await User.findOne({email});
+
+    if(user){
+        throw new ApiError(400, "User already exists");
+    }
+
+    const newUser = await User.create({
+        email,
+        fullName,
+        password,
+    })
+
+    const createdUser = await User.findById(newUser._id).select(
+        "-password -refreshToken"
+    );
+
+    if(!createdUser){
+        throw new ApiError(400, "User not created");
+    }
+
+    const {accessToken} = await generateAccessToken(createdUser._id);
+
+    res.cookie("accessToken", accessToken, options);
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User Created succesfully" ))
+    
+})
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,6 +26,26 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save",async function (){
+   if (!this.isModified("password")) return;
+   this.password = await bcryptjs.hash(this.password, 10);
+})
+
+userSchema.methods.isPasswordMatched = async function(password){
+  return await bcryptjs.compare(password, this.password);
+}
+
+userSchema.methods.generateToken = function(){
+  return jwt.sign({
+     _id: this._id,
+      email: this.email,
+      fullname: this.fullname,
+  },
+  process.env.JWT_SECRET_KEY,
+  {expiresIn: "7d"}
+)
+}
 
 const User = mongoose.model("User", userSchema);
 
