e93b6eb6e0e3c3147c5352209aa9669b3a09e381
Katari <kataricoder@gmail.com>
2025-12-20 00:33:09 +0530

    implement authentication flow with user state management; add AuthImagePattern component and theme constants

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom"
+import React, { useEffect } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
 
+import { Loader } from "lucide-react";
 import Navbar from './components/Navbar'
 import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import Home from './pages/Home'
+import { useAuthStore } from './store/useAuthStore'
+import {Toaster} from 'react-hot-toast'
+
 
 const App = () => {
 
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+
+  console.log(authUser);
+if (isCheckingAuth) {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  );
+}
 
 
   return (
     <div>
       <Navbar/>
       <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/signup" element={<SignUpPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/settings" element={<SettingsPage/>}/>
-      <Route path="/profile" element={<ProfilePage/>}/>
+      <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
     </Routes>
+    <Toaster/>
     </div>
 
     
